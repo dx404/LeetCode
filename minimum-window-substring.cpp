fd061478d419f309d88045c11a0737a02fb6747b
Duo "Donald" Zhao <wisdom.to.pilot@gmail.com>
2014-10-14 16:12:49 -0400

    Update minimum-window-substring.cpp

--- minimum-window-substring.cpp
+++ minimum-window-substring.cpp
@@ -1,38 +1,27 @@
-class Solution {  
-public:  
-    string minWindow(string S, string T) {  
-        int data[260],i,j;  
-        memset(data,0,sizeof(data));  
-        for(i=0;i<T.length();++i)  
-            data[T[i]]++;  
-        int now[260],left,right,minn=1<<29,num=0;  
-        memset(now,0,sizeof(now));  
-        for(i=0,j=0;i<S.length();++i)   
-        {  
-            if(num<T.length())  
-            {  
-                if(now[S[i]]<data[S[i]])num++;  
-                now[S[i]]++;  
-            }  
-            if(num==T.length())  
-            {  
-                while(j<=i&&now[S[j]]-1>=data[S[j]])  
-                {  
-                    --now[S[j]];  
-                    ++j;                      
-                }  
-                if(minn>i-j+1)left=j,right=i,minn=i-j+1;  
-            
-                while(j<=i&&num==T.length())  
-                {  
-                    now[S[j]]--;  
-                    if(now[S[j]]<data[S[j]])num--;  
-                    ++j;  
-                }    
-            }  
-        }  
-        string temp;  
-        if(minn<1<<29)return temp.assign(S,left,right-left+1);  
-        else return "";  
-    }  
-};  
+class Solution {
+public:
+    string minWindow(string S, string T) {
+        string result = "";
+        int n = (int) S.length();
+        unordered_map<char, int> hist;
+        for (auto c : T) hist[c]++;
+        int memoCount = hist.size();
+        int iBest = 0, lenBest = n + 1;
+        for (int i = 0, j = 0; j < n; j++) {
+            hist[S[j]]--; // inclusive
+            if (hist[S[j]] == 0) 
+                memoCount--; 
+            if(memoCount) 
+                continue;
+            while (hist[S[i]]) { // < 0
+                hist[S[i]]++;
+                i++;
+            }
+            if (j - i + 1 < lenBest) {
+                iBest = i;
+                lenBest = j - i + 1;
+            }
+        }
+        return lenBest <= n ? S.substr(iBest, lenBest) : "";
+    }
+};
