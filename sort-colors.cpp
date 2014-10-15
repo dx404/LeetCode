e8c77ddffae76be18227ea4d6e30b31631b7e9bb
Duo "Donald" Zhao <wisdom.to.pilot@gmail.com>
2014-10-15 01:46:54 -0400

    Update sort-colors.cpp

--- sort-colors.cpp
+++ sort-colors.cpp
@@ -1,18 +1,10 @@
 class Solution {
-public:
-    void sortColors(int A[], int n) {
-        int colorCount[3] = {0,0,0};
-        for (int i = 0; i < n; i++){
-            colorCount[A[i]]++;
-        }
-        for(int i = 0; i < colorCount[0]; i++){
-            A[i] = 0;
-        }
-        for(int i = colorCount[0]; i < colorCount[0] + colorCount[1]; i++){
-            A[i] = 1;
-        }
-        for(int i = colorCount[0] + colorCount[1]; i < n; i++){
-            A[i] = 2;
+public: // Multiple partition quick sort
+    void sortColors(int A[], int n) { 
+        for (int i = -1, j = -1, k = 0; k < n; k++) {
+            if (A[k] == 0) swap(A[++i], A[k]);
+            j = max(j, i);
+            if (A[k] == 1) swap(A[++j], A[k]);
         }
     }
 };
